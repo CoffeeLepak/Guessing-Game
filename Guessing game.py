ea0e8368b864a4603c32a64ec312300f32d978ed
CoffeeLepak <zakyrahim6@gmail.com>
2021-07-13 20:01:43 +0800

    Create Guessing game.py

---
+++ Guessing game.py
@@ -0,0 +1,17 @@
+
+secret_word = "apple"
+guess = ""
+guess_count = 0
+guess_limit = 5
+out_of_guesses = False
+
+while guess != secret_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("Out Of Guesses, YOU LOSE! GOOD DAY SIR!")
+else:
+    print("YOU WIN! CONGRATULATIONS!")
